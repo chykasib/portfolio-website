57a5d13faf5229beed6aff4af5892f5d487852bc
Kasib Mohammad Chowdhury <kasib.md.chy@gmail.com>
2023-12-31 00:17:04 +0600

    added aboutSection, heroSection and navbar

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -15,13 +15,22 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+          <h1 className="text-white mb-4 text-3xl sm:text-4xl lg:text-6xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
               Hello, I&apos;m{" "}
             </span>
             <br></br>
             <TypeAnimation
-              sequence={["Kasib", 1000, "Mern Stack Developer"]}
+              sequence={[
+                "Kasib",
+                1000,
+                "Web Developer",
+                1000,
+                "Mobile Developer",
+                1000,
+                "UI/UX Designer",
+                1000,
+              ]}
               wrapper="span"
               speed={50}
               repeat={Infinity}
@@ -42,7 +51,7 @@ const HeroSection = () => {
               href="/"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              <span className="block bg-[#121212] hover:bg-[#4E96E3] rounded-full px-5 py-2">
                 Download CV
               </span>
             </Link>
@@ -54,7 +63,7 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="rounded-full bg-black w-[250px] h-[300px] lg:w-[300px] lg:h-[400px] relative">
             <Image
               src="/images/hero-image.png"
               alt="hero image"
